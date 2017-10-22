28bf9a876077029334a24178dbb40e20a69c9bb7
sulphy <info@loganfox.uk>
2017-10-22 20:12:13 +0100

    Initial check-in of module FizzBuzz

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string _value
+        {
+            get;
+            set;
+        }
+        public FizzBuzz()
+        {
+
+            for (var i = 0; i <= 100; i++){
+
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    PrintResult("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    PrintResult("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    PrintResult("Buzz");
+                }
+                else PrintResult(i.ToString());
+            }
+        }
+
+        private void PrintResult(string value)
+        {
+            _value = value;
+            Console.WriteLine(_value);      
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("FizBuzz Test");
+            var fbObject = new FizzBuzz();
+        }
+    }
+}
